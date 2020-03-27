7e9bfc92431ca224a8366aa859513b13a7b7ec9a
jrpineda <53352598+jrpineda@users.noreply.github.com>
2020-03-27 11:53:02 -0400

    Update Covid.R

--- Covid.R
+++ Covid.R
@@ -10,10 +10,13 @@ today <- today()
 #Update Date below for file
 
 raw <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+raw_death <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 
 covid <- gather(raw, key = "Date", value="Value", -c(`Province/State`,`Country/Region`,Lat,Long))
+covid_death <- gather(raw_death, key = "Date", value="Value", -c(`Province/State`,`Country/Region`,Lat,Long))
 
 covid$Date <- mdy(covid$Date)
+covid_death$Date <- mdy(covid_death$Date)
 
 covid$country <- covid$`Country/Region`
 covid$`Country/Region` <- NULL
@@ -21,12 +24,21 @@ covid$state <- covid$`Province/State`
 covid$`Province/State` <- NULL
 covid$Value <- as.numeric(covid$Value)
 
+covid_death$country <- covid_death$`Country/Region`
+covid_death$`Country/Region` <- NULL
+covid_death$state <- covid_death$`Province/State`
+covid_death$`Province/State` <- NULL
+covid_death$Value <- as.numeric(covid_death$Value)
+
 covid <- covid %>%  
         group_by(country, Date) %>% 
         summarize(Value=sum(Value)) %>% 
         select(country, Date, Value) 
         
-
+covid_death <- covid_death %>%  
+        group_by(country, Date) %>% 
+        summarize(Value=sum(Value)) %>% 
+        select(country, Date, Value)
 
 CHN <- covid %>% 
         filter(country=="China") %>% 
@@ -156,118 +168,42 @@ MEX <- covid %>%
 
 covid_ctry <- rbind(CHN, ITA, USA, IRA, SKA, SPA, GER, FRA, SWI, CRU,
                     UK, NED, JAP, NOR, SWE, BEL, AUS, DEN, MAL, SNG, MEX)
+
+#Create log of value and moving average for rate
+
+ma <- function(x, n = 5){stats::filter(x, rep(1 / n, n), sides = 1)}
+
 covid_ctry <- covid_ctry %>% 
         mutate(lValue = log(Value)) %>% 
         group_by(country) %>% 
-        mutate(rate = (Value-lag(Value))/lag(Value))
-        
-
-qplot(data = covid_ctry, x = Date, y = (Value), color = country)
-
-covid_CHN <- covid_ctry %>% 
-        filter(country=="China")
-
-covid_CHN <- covid_CHN[,3]
-
-covid_USA <- covid_ctry %>% 
-        filter(country=="US") 
-
-covid_USA <- covid_USA[,3]
-
-covid_USA_ts <- as.ts(covid_USA$Value)
-
-
-(AR <- arima(covid_CHN, order = c(2,0,2)))
-residuals <- AR$residuals
-
-covid_CHN_ts <- as.ts(covid_CHN$Value)
-acf(covid_CHN_ts)
-pacf(covid_CHN_ts)
-
-CHN_fitted <- covid_CHN_ts - residuals
-ts.plot(covid_CHN_ts)
-points(CHN_fitted, type = "l", col = 2, lty = 2)
-
-forecast <- predict(AR, n.ahead=30)
-
-ts.plot(covid_CHN_ts, CHN_fitted,forecast$pred, covid_USA_ts, 
-        col=c("black","red", "red", "blue"))
-abline(h=13677, col="grey")
-
-residuals[1] <- 0
-residuals_US <- residuals
-
-USA_fitted <- as.numeric(covid_USA_ts-residuals_US)
-USA_fitted[0:23] <- 0
-
-fit1 <- as.numeric(CHN_fitted[8:29])
-
-USA_fitted[30:51] <- fit1
-USA_fitted <- as.ts(USA_fitted)
-
-ts.plot(covid_CHN_ts, CHN_fitted,forecast$pred, covid_USA_ts, USA_fitted,
-        col=c("black","red", "red", "blue", "green"))
-
-fit1<- as.numeric(covid_CHN_ts)
-fit1[23:70] <- NA
-fit2 <- as.numeric(CHN_fitted)
-fit2[29:70] <- last(fit2)
-fit3 <- as.numeric(forecast$pred)
-fit3[0:30] <- NA
-fit3[60:70] <- last(forecast$pred)
-fit4 <- as.numeric(covid_USA_ts)
-fit4[30:70] <- NA
-fit5 <- as.numeric(USA_fitted)
-fit5[52:70] <- last(fit5)
-
-date <- ymd("2020-01-22")
-
-time <- 1:140
-time <- time[c(TRUE, FALSE)]
-
-final_df<- data.frame(fit1,fit2,fit3,fit4,fit5, days =time)
-
-
-
-final_df$date <- ymd("2020-01-22")+ final_df$days
-final_df$fit3 <- NULL
-
-final_df2 <- final_df %>% 
-        gather(key = type, value = value, -date, -days) %>% 
-        mutate(group = ifelse(type=="fit1", "China Actual",
-                      ifelse(type=="fit2", "China Forecast",
-                             ifelse(type=="fit4", "US Actual",
-                                    "US Forecast"))))
-
-
-ggplot()+
-        geom_line(data = final_df2[final_df2$type=="fit1",], aes(x = date, y = value, color = group), size = 5)+
-        geom_line(data = final_df2[final_df2$type=="fit2",], aes(x = date, y = value, color = group), size = 2)+
-        geom_line(data = final_df2[final_df2$type=="fit4",], aes(x = date, y = value, color = group), size = 5)+
-        geom_line(data = final_df2[final_df2$type=="fit5",], aes(x = date, y = value, color = group), size = 2)+
-        labs(title="Coronavirus Model - USA vs China", x = "Date", caption = "Source: Humanitarian Data Exchange",
-             subtitle = "By JRP (2020/03/20)")+
-        theme_economist()+
-        scale_y_continuous(name="Confirmed Cases", labels = comma)+
-        theme(legend.title=element_blank())
-
+        mutate(rate = (Value-lag(Value))/lag(Value)) %>% 
+        mutate(mov_avg = ma(rate, n = 10))
 
 #Covid Maps
 
 covid_ctry %>%
-        
-        filter(country==c("China", "Italy", "US", "Mexico")) %>% 
-        
-        
-        ggplot(aes(x = Date, y = lValue, group_by(country), color = country))+
-        geom_smooth()
+        filter(country %in% c("China", "Italy", "US", "Mexico", "Spain", "United Kingdom")) %>% 
+        filter(rate<1) %>% 
+        ggplot(aes(x = Date, y = rate, group_by(country), color = country))+
+        stat_smooth(se=FALSE, method = "loess", size = 1.5)+
+        scale_y_continuous(name="Percent Change in cases (smoothed)", labels=percent)+
+        labs(title = "Smoothed Rate of Change in Cases")
 
 covid_ctry %>%
-        
-        filter(country==c("China", "Italy", "US", "Mexico")) %>% 
-        
+                filter(country %in% c("China", "Italy", "US", "Mexico", "Spain", "United Kingdom")) %>% 
+                ggplot(aes(x = Date, y = lValue, group_by(country), color = country))+
+                geom_line(size =1.5)+
+        scale_y_continuous(name="Logarithmic Confirmed Cases by Country", labels=comma)+
+        labs(title = "Logarithmic Scaled Value ")
+
+
+covid_ctry %>%
+        filter(country %in% c("China", "Italy", "US", "Mexico", "Spain", "Japan", "Iran", "Germany")) %>% 
         ggplot(aes(x = Date, y = Value, group_by(country), color = country))+
-        geom_line(size = 1)
+        geom_line(size = 1.5)+
+        scale_y_continuous(name = "Confirmed Cases", labels=comma)+
+        labs(title = "Total Confirmed Cases")
+
 
 #Rate graph
 
@@ -284,12 +220,11 @@ covid_ctry %>%
 
 ttd <- covid_ctry %>% 
         group_by(country) %>% 
-        filter(rate<Inf & rate>-Inf) %>% 
         arrange(desc(Date)) %>% 
         filter(row_number()==1) %>% 
-        mutate(ttd = 72/(rate*100)) %>% 
-        mutate(pred_case=(Value*(1+rate)^14)) %>% 
-        mutate(pred_case3=(Value*(1+rate)^21))
+        mutate(ttd = 72/(mov_avg*100)) %>% 
+        mutate(pred_case=(Value*(1+mov_avg)^14)) %>% 
+        mutate(pred_case3=(Value*(1+mov_avg)^21)) 
         
 
 ttd %>% 
@@ -348,4 +283,23 @@ three_week_forecast %>%
         xlab ("Country")+
         scale_fill_discrete(name = "Cases", labels = c("Predicted", "Current"))+
         scale_y_continuous(name="Number of cases", labels=comma)
+
+
+covid_death %>% 
+        filter(country %in% c("China", "US", "Mexico", "Spain", "Italy", "United Kingdom")) %>% 
+        filter(Date==last(Date)) %>% 
+        arrange(desc(Value)) %>% 
+
+ggplot(aes(x = country, y = Value, fill = country))+
+        geom_bar(stat = "identity")+
+        labs(title = "Deaths by Country")
+
+covid_ctry %>% 
+        filter(Date>"2020-03-01", country =="US") %>% 
+        mutate(slope = (lValue - lag(lValue))/lag(lValue)) %>% 
+        ggplot(aes(x = Date, y=slope))+
+        geom_line()+
+        geom_smooth()+
+        labs(title = "Rate of Change in log(Value) for USA")
+        
 
